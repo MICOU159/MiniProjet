beedde197eadccb9fc1ef9a5640ede42843ffcf5
MICOU159 <michel.couture.3@ulaval.ca>
2014-04-16 03:46:18 -0400

    Ajout Google API
    Ajout de l'api de google maps ainsi que la clée requise et permissions
    pour l'app dans le manifeste.
    Suppression de example\miniprojet
    
    google api key contient l'information de ma clé. Si jamais ça ne marche
    pas de ton côté ce sera peut être parce que ton debug.keystore n'est pas
    le même que moi (je ne suis pas certains que ça suit lorsqu'on fait les
    commit.

--- src/ca/ulaval/ima/miniprojet/MainActivity.java
+++ src/ca/ulaval/ima/miniprojet/MainActivity.java
@@ -3,6 +3,7 @@ package ca.ulaval.ima.miniprojet;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+//import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,17 +12,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class MainActivity extends Activity {
 
+public class MainActivity extends Activity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+
 		}
+
 	}
 
 	@Override
